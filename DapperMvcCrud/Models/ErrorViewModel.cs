4080401c57ebef955dd0d6594891c405b68550eb
C2ngo <158284545+C2ngo@users.noreply.github.com>
2024-01-31 20:01:38 +0200

    Add files via upload
    inti commit

---
+++ DapperMvcCrud/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace DapperMvcCrud.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
+
